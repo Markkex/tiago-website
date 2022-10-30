09c71783d3a4fca650ab564028f4bf4357abb8f2
Marco Ávila <avila.marco.dev@gmail.com>
2022-10-30 23:39:11 +0000

    implement side content container

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -12,6 +12,7 @@ import "./index.css";
 import ResponsiveAppBar from "../../components/Menu";
 import { Container } from "@mui/system";
 import towel from "../../assets/towel.png";
+import SideContent from "../../components/side-content/SideContent";
 //images
 
 const Home = () => {
@@ -159,6 +160,25 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <SideContent
+        titleText={"Casas da Vila"}
+        titleDescription={
+          "O AGRUPAMENTO IDEAL PARA DIAS DE TRANQUILIDADE EM FUZETA"
+        }
+        textOne={`Casas da Vila é um agrupamento de casas com destino a arrendamento em Fuseta, no Algarve.`}
+        textTwo={`A 2 minutos da praia e ilha da Fuzeta esta é uma localização
+                priveligiada sobre o Parque Natural da Ria Formosa, eleita uma
+                das 7 Maravilhas Naturais de Portugal, estes alojamentos são o
+                local ideal para umas férias em familia e ficar a conhecer o
+                sotavento algarvio.`}
+        textThree={`Localizado no centro da vila da Fuzeta, estamos completamente
+                equipados para a sua estadia conosco. Ao reservar conosco também
+                tem direito a descontos com os nossos parceiros.`}
+        textFour={`As Casas da Vila encontram-se a 20km da cidade de Faro e da
+                cidade de Tavira, sendo o sitio perfeito para os grandes polos
+                do sotavento algarvio.`}
+        imageUrl={towel}
+      />
     </div>
   );
 };
